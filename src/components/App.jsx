f84d98cc41263882a1457394b96530d50fe1aa76
Roman <sayga76239@gmail.com>
2023-03-01 13:23:27 +0100

    polomka

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 
 export const App = () => {
+  const [counter, setCounter] = useState(0);
 
+  function increment() {
+    // setCounter(counter + 1);
+    setCounter((prev) => {
+      return prev + 1;
+    })
+  }
+  
+  function decrement() {
+    setCounter(counter - 1);
+  }
+  
+  return (
+    <div>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment} className="btn btn-success">Добавить</button>
+      
+      <button onClick={decrement} className="btn btn-danger">Уменьшить</button>
+  
+    </div>
+  );
  
 
-  return (  
-    <div>
-      
-    
-    </div>
   
-  );
 };
