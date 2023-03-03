9ca4d552e21b4fbca734866eef677a7d754c4b98
Roman <sayga76239@gmail.com>
2023-03-03 15:57:23 +0100

    finalLink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
-import { Route, Routes, NavLink } from "react-router-dom";
-// import styled from "styled-components";
-import { About } from "../pages/About";
-import { Home } from "../pages/Home";
-import { ProductDetails } from "../pages/ProductDetails";
-import { Products } from "../pages/Products";
-import { Mission } from "./Mission";
-import { Team } from "./Team";
-import { Reviews } from "./Reviews";
-import { Container, Header, Logo, Link } from "./App.styled";
-
+import { Route, Routes } from 'react-router-dom';
+import styled from "styled-components";
+import { About } from '../pages/About';
+import { Home } from '../pages/Home';
+import { ProductDetails } from '../pages/ProductDetails';
+import { Products } from '../pages/Products';
+import { Mission } from './Mission';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
+import { SharedLayot } from "./SharedLayout";
 // const StyledLink = styled(NavLink)`
 //   color: black;
 
@@ -18,32 +17,17 @@ import { Container, Header, Logo, Link } from "./App.styled";
 // `;
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{" "}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/" end>
-            Home
-          </Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-      </Routes>
-    </Container>
+    <Routes>
+      <Route path="/" element={<SharedLayot />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />}>
+        <Route path="mission" element={<Mission />} />
+        <Route path="team" element={<Team />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="products" element={<Products />} />
+      <Route path="products/:productId" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
